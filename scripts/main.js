90f0e195c9c3e51f37ae60f3c87253dbf42e1545
Mpho Lebona <giftedmpho99@gmail.com>
2026-01-15 15:12:37 +0200

    Add mobile menu for responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,17 @@
 const currentYear = new Date().getFullYear(); //access the current year from the Date object.
 let footerDate = document.getElementById('date'); //get the DOM element with id 'date'
 footerDate.textContent = currentYear; //set the text content of the element to the current year
+
+
+// Mobile navigation menu toggle
+const navToggle = document.getElementById('mobile-menu-btn'); //select the element with class 'nav-toggle'
+const mobileMenu = document.getElementById('mobile-dropdown');
+const closeMenu = document.getElementById('close-menu');
+
+navToggle.addEventListener('click', () => {
+  mobileMenu.classList.remove('hidden'); //remove the 'hidden' class to show the menu
+});
+
+closeMenu.addEventListener('click', () => {
+  mobileMenu.classList.add('hidden'); //add the 'hidden' class to hide the menu
+});
